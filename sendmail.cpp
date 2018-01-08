f083117c540856df863b6549831a420816269af9
duzypit <piatek.tomasz@gmail.com>
2018-01-08 14:10:15 +0100

    added password class

--- sendmail.cpp
+++ sendmail.cpp
@@ -11,8 +11,30 @@
 #include "socket.cpp"
 #include "myopenssl.cpp"
 #include "mysmtp.cpp"
+#include "passwd.cpp"
+
+
 int main ()
 {
+    //get username
+    std::cout << "Gmail username: ";
+    std::string uname;
+    std::cin >> uname;
+    //get pass
+    std::cout << "password: ";
+    Passwd p;
+    std::string password;
+    password = p.getPassword();
+    std::cout << std::endl;
+    //get from
+    std::string from(uname);
+    from += "@gmail.com";
+
+    //get to
+    std::cout << "Recipient: ";
+    std::string recipient;
+    std::cin >> recipient;
+
     // This needs to be called only once.
     // Don't let multi-threads run it several times.
     MyOpenSSL::StaticInitialize ssInitializer;
@@ -23,10 +45,10 @@ int main ()
     MySMTP::sendSSL (
         "smtp.gmail.com", // server address
         465, // port
-        "your_user_name", // userName
-        "pass", // password
-        "your_user_name@gmail.com", // from
-        "recipient@aa.com", // to
+        uname, // userName
+        password, // password
+        from, // from
+        recipient, // to
         "Testing Boost STMP", // subject
         "This message body" // message
     );
