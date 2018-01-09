266addf8a9d8dd5ddda04922eff6e65cec39d7d8
duzypit <piatek.tomasz@gmail.com>
2018-01-09 13:28:03 +0100

    boost removed, minor rewrite & name change

--- include/smtpSender.cpp
+++ include/smtpSender.cpp
@@ -1,23 +1,21 @@
 #ifndef __MYSMTP_CPP__
 #define  __MYSMTP_CPP__
-#include <openssl/bio.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-#include "myopenssl.cpp"
+//#include <openssl/bio.h>
+//#include <openssl/ssl.h>
+//#include <openssl/err.h>
+#include "openSSLBearer.cpp"
 #include <iostream>
 #include <string>
-//#include <boost/archive/iterators/base64_from_binary.hpp>
-//#include <boost/archive/iterators/transform_width.hpp>
-//#include "socket.cpp"
-#include "practicalSocket.h"
-#include "practicalSocket.cpp"
+#include "smtpSocket.cpp"
 #include "base64.h"
 
-class MySMTP
+class SMTPSender
 {
 public:
-    static void sendSSL (const std::string& server,
-                        uint16_t port,
+    SMTPSender() : newline("\r\n"){}
+
+    void sendSSL (const std::string& server,
+                        int port,
                         const std::string& userName,
                         const std::string& password,
                         const std::string& from,
@@ -25,35 +23,44 @@ public:
                         const std::string& subject,
                         const std::string& message)
     {
-        static const std::string newline = "\r\n";
-        Socket socket(server, port);
-        MyOpenSSL openSSL (socket.GetSocket()->nativeHandle());
+        SMTPSocket socket(server, port);
+        OpenSSLBearer openSSL (socket.GetSocket()->nativeHandle());
         // http:en.wikipedia.org/wiki/Simple Mail Transfer ProtocolitSMTP transport example
-        //std::cout << openSSL.Read (ReceiveFunctor (220));// 220 mx. google.com ESMTP
+        std::cout << openSSL.Read (ReceiveFunctor (220));// 220 mx. google.com ESMTP
+
         openSSL.Write(std::string("EHLO ") + server + newline);
-        //std::cout << openSSL.Read (ReceiveFunctor (250));	// 250-mx.google.com at your service,
+        std::cout << openSSL.Read (ReceiveFunctor (250));	// 250-mx.google.com at your service,
+
         openSSL.Write (std::string ("AUTH LOGIN") + newline);
-        //std::cout << openSSL.Read ( ReceiveFunctor (334) ); // 334
+        std::cout << openSSL.Read ( ReceiveFunctor (334) ); // 334
+
         openSSL.Write (EncodeBase64(userName) + newline);
-        //std::cout << openSSL.Read (ReceiveFunctor (334) ); // 334
+        std::cout << openSSL.Read (ReceiveFunctor (334) ); // 334
+
         openSSL.Write(EncodeBase64(password) + newline);
-        //std::cout << openSSL.Read ( ReceiveFunctor ( 235 ) ); // 235 2.7.0 Accepted
+        std::cout << openSSL.Read ( ReceiveFunctor ( 235 ) ); // 235 2.7.0 Accepted // 535 not accepted
+
         openSSL.Write(std::string ("MAIL FROM: <") + from + " >" + newline);
-        //std::cout << openSSL.Read (ReceiveFunctor (250));	// 250 2.1.0 0K
+        std::cout << openSSL.Read (ReceiveFunctor (250));	// 250 2.1.0 0K
+
         openSSL.Write (std::string("RCPT To:<") + to + ">" + newline);
-        //std::cout << openSSL.Read (ReceiveFunctor (250)); // 250 2.1.5 0K
+        std::cout << openSSL.Read (ReceiveFunctor (250)); // 250 2.1.5 0K
+
         openSSL.Write (std::string("DATA") + newline);
-        //std::cout << openSSL.Read ( ReceiveFunctor (354) ); // 354 Go ahead
+        std::cout << openSSL.Read ( ReceiveFunctor (354) ); // 354 Go ahead
+
         openSSL.Write(std::string("From: <") + from + ">" + newline);
         openSSL.Write (std::string("To: <") + to + ">" + newline);
         openSSL.Write(std::string("SUBJECT: ") + subject + newline);
         openSSL.Write (newline + message + newline + "." + newline);
-        //std::cout << openSSL.Read ( ReceiveFunctor (250) ) << std::endl; // 250 2.0.0 OK
+
+        std::cout << openSSL.Read ( ReceiveFunctor (250) ) << std::endl; // 250 2.0.0 OK
         openSSL.Write (std::string("QUIT") + newline);
         std::cout << openSSL.Read (ReceiveFunctor (221));	// 22l 2.0.0 closing connection
     }
 
 private:
+    std::string newline;
     struct ReceiveFunctor
     {
         enum { codeLength = 3 };
